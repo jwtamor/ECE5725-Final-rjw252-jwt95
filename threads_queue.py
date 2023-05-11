b06e915200c37f7ceaaa2576a1ad3f7ef4fd5bce
jwtamor <jwt95@cornell.edu>
2023-05-11 15:01:30 -0400

    Comments

--- threads_queue.py
+++ threads_queue.py
@@ -4,41 +4,31 @@ import sys
 import time
 from multiprocessing import Process, Queue, Value, Lock, Array
 
-def process1(y, x):
-	for i in range(0,10):
-		x = x + 1
-	y.value = x
-
-def process0(y, x):
-	for i in range(0,10):
-		x = x + 1
-	y.value = x
+"""
+Rosie Wildermuth (rjw252) and Jacob Tamor (jwt95)
+Final Project: RPi DJ
+04/11/2023
+Thread code that runs both interface_queue and preprocessing_queue simultaneously
+and instantiates their shared queues. Run this code to run the entire system.
+"""
 
 if __name__ == '__main__':
-	# ~ sum0 = Value('i', 0)
-	# ~ sum1 = Value('i', 0)
 	
+	# Create Queues
 	ctx = mp.get_context('spawn')
 	q = ctx.Queue()
 	p = ctx.Queue()
 	
-	# ~ p0 = Process(target=interface.main)
+	# Create Processes
 	p0 = Process(target=interface_queue.main, args=(q,p))
-	# ~ p1 = Process(target=process1, args=(sum1,5))
 	p1 = Process(target=preprocessing_queue.main, args=(q,p))
 	
+	# Start Processes
 	p0.start()
 	p1.start()
 	
-	
+	# Join Processes
 	p1.join()
-	
 	p0.join()
-	
-	# ~ print("DONE")
-	# ~ print(sum0.value)
-	# ~ print(sum1.value)
-	# ~ time.sleep(3)
-	sys.exit()
-	
-	
+
+	sys.exit()	
