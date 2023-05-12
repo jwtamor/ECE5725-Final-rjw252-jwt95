8ad1fec5dc9f7dddcaae29966e25da3823ab381d
Jacob Tamor <77250477+jwtamor@users.noreply.github.com>
2023-05-12 17:12:45 -0400

    Update preprocessing_queue.py

--- preprocessing_queue.py
+++ preprocessing_queue.py
@@ -142,7 +142,7 @@ def main(q,p):	# Preprocessing Main Function
 			processed = process(change_volume(bucket * modulate(b,m,t), v))
 			for frame in processed: 	# Output Bucket
 				# ~ print("Writing...")
-				output = 0x3000 | (int(frame) & 0x0FFF)
+				output = 0x3000 | (int(frame) >> 4)
 				output_bytes = (output).to_bytes(2, 'big', signed=True)
 				spi.writebytes(output_bytes)
 		song.close()
